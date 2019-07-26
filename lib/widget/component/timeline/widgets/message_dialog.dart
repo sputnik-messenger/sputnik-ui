aedc3fc3e9c8b27cae78010939550b057ebf32d9
Mohammed El Batya <elbatya@googlemail.com>
2019-07-27 00:02:08 +0200

    adding copyright notice

--- lib/widget/component/timeline/widgets/message_dialog.dart
+++ lib/widget/component/timeline/widgets/message_dialog.dart
@@ -1,3 +1,21 @@
+// Copyright (C) 2019 Mohammed El Batya
+//
+// This file is part of sputnik_ui.
+//
+// sputnik_ui is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:matrix_rest_api/matrix_client_api_r0.dart';
